9b6a9d4c529edcb368c6bb2ffbb78d92b53ecdbb
Sebasflm <122398132+Sebasflm@users.noreply.github.com>
2026-05-07 13:25:08 -0500

    Add files via upload

---
+++ Ejercicios/Bucles/While/listas_ej3.py
@@ -0,0 +1,12 @@
+numero = int(input("Por favor ingrese un número positivo: \n"))
+
+resultado = 1
+contador = 1
+
+while contador <= numero:
+    resultado = resultado * contador
+    contador +=1
+    
+print(resultado) 
+
+
---
+++ Ejercicios/Bucles/While/listas_ej4.py
@@ -0,0 +1,9 @@
+
+while True:
+    contraseña = input("Por favor ingresar la respectiva contraseña: \n")
+    if len(contraseña) >= 8:
+        print("Contraseña válida")
+        break
+    else:
+        print("Ingrese una contraseña válida") 
+
