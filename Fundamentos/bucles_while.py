96117f43e84a3b7c0572f144ede623967eb990a6
Sebasflm <122398132+Sebasflm@users.noreply.github.com>
2026-05-07 13:26:09 -0500

    Add files via upload

---
+++ Fundamentos/bucles_while.py
@@ -0,0 +1,17 @@
+
+print("\nBucle while con condicion")
+contador = 0 
+while contador < 5: 
+    print(contador)
+    contador += 1
+
+contador = 0 
+
+print("\nBucle while con break")
+while True: 
+    print(contador)
+    contador += 1
+    if contador == 5:
+        break
+
+    
